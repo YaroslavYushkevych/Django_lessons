d5129c1788a00e62584431986417fa717089cfc3
YaroslavYushkevych <yaroslavyushkevych@gmail.com>
2023-05-10 15:58:20 +0300

    New path

--- djsite/testsite/women/views.py
+++ djsite/testsite/women/views.py
@@ -4,32 +4,45 @@ from django.shortcuts import render, redirect
 from .models import *
 
 # Create your views here.
-menu = ["Про сайт", "Додати статтю", "Зворотній зв'язок", "Ввійти"]
+menu = [{'title': "Про сайт", 'url_name': 'about'},
+        {'title': "Додати статтю", 'url_name': 'add_page'},
+        {'title': "Зворотній зв'язок", 'url_name': 'contact'},
+        {'title': "Зайти", 'url_name': 'login'}
+]
+
 
 def index(request):
     posts = Women.objects.all()
-    return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Головна сторінка'})
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Головна сторінка'
+    }
+
+    return render(request, 'women/index.html', context=context)
 
 
 def about(request):
     return render(request, 'women/about.html', {'menu': menu, 'title': 'Про сайт'})
 
 
-def categories(request, catid):
-    #Перевірка чи щось було записано
-    if(request.GET):
-        print(request.GET)
-    return HttpResponse(f"<h1>Статті по категоріям</h1>{catid}</p>")
+def addpage(request):
+    return HttpResponse("Додавання статті")
 
-def archive(request, year):
-    """Виклик помилки
-    if(int(year) > 2023):
-        raise Http404()"""
-    if (int(year) > 2023):
-        #Тимчасовий 302   redirect('/')
-        #Постійний  301   redirect('/', permanent=True)
-        return redirect('home', permanent=True)
-    return HttpResponse(f"<h1>Архів по рокам</h1>{year}</p>")
+
+def contact(request):
+    return HttpResponse("Зворотній зв'язок")
+
+
+def login(request):
+    return HttpResponse("Авторизация")
+
+
+def show_post(request, post_id):
+    return HttpResponse(f"Відображення статті с id = {post_id}")
+
+
+
 
 
 
